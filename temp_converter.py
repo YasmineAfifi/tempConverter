17403ea1e837454cadeb671e92489212983feb18
Yasmine <ynremotecoders@gmail.com>
2023-10-05 16:13:48 +0300

    new version temp converter

--- temp_converter.py
+++
@@ -1,9 +0,0 @@
-
-def temp_convert():
-    celsius = input("please enter the celsius degree : ")
-    result = (float(celsius) * 9/5) + 32
-    return celsius +" degrees Celsius are "+str(result)+" degrees Farenheit."
-
-
-
-print(temp_convert())
